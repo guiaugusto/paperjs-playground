1ea77a3c528e969cf2f08a28f69ca5c750a3e14c
guiaugusto <guilherme.francais@gmail.com>
2020-11-15 15:10:58 -0300

    Creates simple animation and fix commands
    - Built onFrame method from path
    - Build point when forward are called
    - Change left to right (code wrongly)

--- example.js
+++ example.js
@@ -2,35 +2,49 @@
 paper.install(window);
 paper.setup('myCanvas')
 
+class Screen {
+    constructor(width = window.innerWidth, height = window.innerHeight) {
+        this.width = width;
+        this.height = height;
+
+        this.setCanvasArea = (width, height) => {
+            var canvas = document.getElementById('myCanvas');
+            canvas.width = this.width;
+            canvas.height = this.height;
+        }
+
+        this.setCanvasArea(this.width, this.height);
+    }
+}
+
+let sleep = (time, delta) => {
+    for(let i = 0; i < time*delta; i++);
+}
+
+let seg = 0;
+
 // creates general path
 var path = new Path();
 path.strokeColor = 'black';
 
-// follows canvas orientation
-// x - and y +  |  x + and y +
-// _____________|_____________
-//              | 
-// x - and y -  |  x + and y -
-function get_orientation(angle) {
-    if (angle >= 0 && angle < 90) return [1, 1];
-    else if (angle >= 90 && angle < 180) return [-1, 1];
-    else if (angle >= 180 && angle < 270) return [-1, -1];
-    else return [1, -1];
-}
-
 function calculate_point(xa, ya, angle, distance) {
     let ca = distance * Math.cos(angle * (Math.PI/180));
     let co = distance * Math.sin(angle * (Math.PI/180));
+    
+    return [xa + ca, ya + co];
+}
 
-    let direction = get_orientation(angle);
-
-    return [xa + ca * direction[0], ya + co * direction[1]];
+function calculate_distance(first_point, second_point) {
+    return Math.sqrt(
+        Math.pow(second_point.x - first_point.x) +
+        Math.pow(second_point.y - first_point.y)
+    )
 }
 
 class Turtle {
-    constructor(x = 0, y = 0) {
-        this.x = x;
-        this.y = y;
+    constructor(x = window.innerWidth / 2, y = window.innerHeight / 2) {
+        this.x = Math.round(x);
+        this.y = Math.round(y);
         this.angle = 0;
         
         // creates a command list that will store each
@@ -45,27 +59,43 @@ class Turtle {
     }
 
     draw() {
-        path.removeSegments();
-
-        this.pathSet.forEach(element => {
-            path.add(element)
-        });
+        let seg = 0;
+        path.onFrame = (event) => {
+            if (seg < this.pathSet.length) path.add(this.pathSet[seg++]);
+        };
     }
 
-    right(received_angle) {
-        this.angle = (360 - this.angle - received_angle) % 360;
+    left = (received_angle) => {
+        this.angle = (this.angle - received_angle) % 360;
+        if (this.angle < 0) this.angle += 360;
     }
 
-    left(received_angle) {
+    right = (received_angle) => {
         this.angle = (this.angle + received_angle) % 360;
+        if (this.angle < 0) this.angle += 360;
     }
 
     forward(distance) {
-        let last_point = calculate_point(this.x, this.y, this.angle, distance);
-
-        this.pathSet.push(
-            new Point(...last_point)
-        );
-        this.update_turtle(...last_point);
+        for (let i = 0; i < distance; i++){
+            let last_point = calculate_point(this.x, this.y, this.angle, 1);
+    
+            this.pathSet.push(
+                new Point(...last_point)
+            );
+            this.update_turtle(...last_point);
+        }
     }
+
+    backward = (distance) => {
+        for (let i = 0; i < distance; i++) {
+            let last_point = calculate_point(this.x, this.y, this.angle, -1);
+
+            this.pathSet.push(
+                new Point(...last_point)
+            );
+            this.update_turtle(...last_point);
+        } 
+    };
 }
+
+new Screen();
